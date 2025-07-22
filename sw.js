0a6b233bd5004908ee31ef9c636db4e63c277bda
Goodzone2355 <chulskij1996@gmail.com>
2025-07-22 16:04:32 +0300

    last commit

--- sw.js
+++ sw.js
@@ -41,6 +41,7 @@ self.addEventListener('fetch', event => {
     event.respondWith(cacheFirst(event.request));
 });
 
+
 async function cacheFirst(request) {
     const cached = await caches.match(request);
     return cached ?? await fetch(request);
